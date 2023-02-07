c4afddb4477164e3287d292e01be85c82c04e2f0
arunbingari <arunbingari@gmail.com>
2023-02-08 02:03:26 +0530

    added all files of the project

---
+++ terraform/variables.tf
@@ -0,0 +1,21 @@
+variable region {
+    default = "ap-south-1"
+}
+variable vpc_cidr_block {
+    default = "10.0.0.0/16"
+}
+variable subnet_cidr_block {
+    default = "10.0.10.0/24"
+}
+variable avail_zone {
+    default = "ap-south-1a"
+}
+variable env_prefix {
+    default = "dev"
+}
+variable my_ip {
+    default = "106.213.81.200/32"
+}
+variable instance_type {
+    default = "t2.micro"
+}
